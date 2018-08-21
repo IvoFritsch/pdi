438bb6e052211d11782efa0b03679682715a4960
Ivo Averbeck Fritsch <0186779@feevale.br>
2018-08-21 20:49:04 -0300

    Adicionada função de tranformação com matriz

---
+++ src/model/Utils.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author 0186779
+ */
+public class Utils {
+    
+    public static BufferedImage aplicaMatrizImagem(BufferedImage inp, double[][] matriz){
+        
+        BufferedImage saida = new BufferedImage(inp.getWidth(), inp.getHeight(), BufferedImage.TYPE_INT_RGB);
+        
+        Imagem imagem = new Imagem(inp);
+        imagem.percorrePixelsImagem(p -> {
+            
+            int xFinal = (int)(matriz[0][0] * p.x + matriz[0][1] * p.x);
+            int yFinal = (int)(matriz[1][0] * p.y + matriz[1][1] * p.y);
+            saida.setRGB(xFinal, yFinal, p.cor.getRGB());
+        });
+        return saida;
+    }
+    
+}
